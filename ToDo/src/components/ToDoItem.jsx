287fd6195a94f79eca5152a674902c37f9305f41
im-nilesh <nileshdadab@gmail.com>
2026-05-29 13:08:42 +0530

    refactor: enhance ToDoItem component with editing, completion, and deletion functionalities

--- ToDo/src/components/ToDoItem.jsx
+++ ToDo/src/components/ToDoItem.jsx
@@ -1,9 +1,43 @@
-function ToDoItem({ item }) {
+import { useState } from "react";
+
+function ToDoItem({ item, deleteTodo, markCompleted, editTodo }) {
+  const [editText, setEditText] = useState(item.desc);
+
+  function handleEdit() {
+    if (editText.trim() === "") {
+      return;
+    }
+
+    editTodo(item.id, editText);
+  }
+
   return (
-    <div>
-      <li>{item.id}</li>
-      <li>{item.desc}</li>
-      <li>{item.status}</li>
+    <div className="todo-item">
+      <h3>{item.desc}</h3>
+
+      <p className={item.status === "Done" ? "status-done" : "status-pending"}>
+        Status : {item.status}
+      </p>
+
+      <input
+        type="text"
+        value={editText}
+        onChange={(e) => setEditText(e.target.value)}
+      />
+
+      <div>
+        <button className="edit-btn" onClick={handleEdit}>
+          Edit
+        </button>
+
+        <button className="complete-btn" onClick={() => markCompleted(item.id)}>
+          Complete
+        </button>
+
+        <button className="delete-btn" onClick={() => deleteTodo(item.id)}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
