609e9f5d840f6f67f531a56dbcc7ecc0872c817d
im-nilesh <nileshdadab@gmail.com>
2026-05-29 13:09:00 +0530

    refactor: update ToDoList component to include add, delete, mark completed, and edit functionalities

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -13,15 +13,59 @@ function App() {
     {
       id: 2,
       desc: "Hello",
-      status: "Done",
+      status: "Pending",
     },
   ];
+
   const [todos, setTodos] = useState(todoList);
+
+  function addTodo(task) {
+    const newTodo = {
+      id: Date.now(),
+      desc: task,
+      status: "Pending",
+    };
+
+    setTodos([...todos, newTodo]);
+  }
+
+  function deleteTodo(id) {
+    const updatedTodos = todos.filter((item) => item.id !== id);
+    setTodos(updatedTodos);
+  }
+
+  function markCompleted(id) {
+    const updatedTodos = todos.map((item) => {
+      if (item.id === id) {
+        return { ...item, status: "Done" };
+      }
+      return item;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  function editTodo(id, newDesc) {
+    const updatedTodos = todos.map((item) => {
+      if (item.id === id) {
+        return { ...item, desc: newDesc };
+      }
+      return item;
+    });
+
+    setTodos(updatedTodos);
+  }
 
   return (
     <>
       <Header />
-      <ToDoList todos={todos} />
+      <ToDoList
+        todos={todos}
+        addTodo={addTodo}
+        deleteTodo={deleteTodo}
+        markCompleted={markCompleted}
+        editTodo={editTodo}
+      />
     </>
   );
 }
